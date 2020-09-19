827023df483ffd8492b4a318a10c83ab0a6b9181
Andrew Bryant <bryant.andrew01@gmail.com>
2020-09-19 15:21:50 -0300

    19 Sep 2020

---
+++ 200913k/sketch.js
@@ -0,0 +1,11 @@
+setup=_=>{
+  createCanvas(w=600,w)
+  background(0)
+  stroke(255)
+  fill(255)
+  for(x=0;x<w;x++){
+    for(y=0;y<w;y++){
+      if((x^y)%(x+y))point(x,y)
+    }
+  }
+}
---
+++ 200919e/sketch.js
@@ -0,0 +1,12 @@
+setup=_=>{
+  createCanvas(w=600,w)
+  background(0)
+  stroke(255)
+  fill(255)
+  for(x=0;x<w;x++){
+    for(y=0;y<w;y++){
+      a=radians(x^y)
+      point(x+cos(a),y+sin(a))
+    }
+  }
+}
